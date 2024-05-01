5810b1b948d4faf38cc2049d08bb969a8a98cdc0
Joeltabe <joeltabe3@gmail.com>
2024-05-01 11:15:49 +0100

    changes to user-route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -20,26 +20,34 @@ router.post('/register', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
 // Login user
 router.post('/login', async (req, res) => {
-    try {
-      const user = await User.findOne({ username: req.body.username });
+  try {
+      const { username, password } = req.body;
+
+      // Find user by username
+      const user = await User.findOne({ username });
+
       if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+          return res.status(404).json({ message: 'User not found' });
       }
-      const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
       if (!passwordMatch) {
-        return res.status(401).json({ message: 'Invalid password' });
+          return res.status(401).json({ message: 'Invalid password' });
       }
+
       // Generate JWT token
       const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
       // Send JWT token to the client
       res.json({ message: 'Login successful', token });
-    } catch (error) {
+  } catch (error) {
       res.status(500).json({ error: error.message });
-    }
-  });
+  }
+});
+
   
 
 module.exports = router;
